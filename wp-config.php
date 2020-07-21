49fd23c3e92d156b0a32611890ae45313e84bafb
Leandro Cabral <68610108+LeandroCabralSec@users.noreply.github.com>
2020-07-21 20:55:40 -0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,3 +1,9 @@
+Uma dica rápida que ajuda a restringir o acesso aos plug-ins e do tema a atualizar o recurso no WordPress, além de remover a capacidade de os 
+usuários instalarem novos plug-ins e temas.
+
+A quick tip that helps restrict access to plug-ins and the theme to update the feature on WordPress, in addition to removing the ability for 
+users to install new plug-ins and themes.
+
 /** # DESABILITAR INSTALAÇÃO DE PLUGINS E TEMAS # */
 / ** # DISABLE PLUGIN AND THEME INSTALLATION # * /
 define('DISALLOW_FILE_MODS', true);
